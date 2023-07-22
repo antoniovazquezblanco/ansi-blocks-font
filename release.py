886c3a5d4147707d284ad52b82d5b43356e6866a
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2023-07-22 15:50:09 +0200

    Add a css deployment file

--- release.py
+++ release.py
@@ -3,6 +3,7 @@
 ANSI blocks font release script.
 '''
 
+import shutil
 import fontforge
 
 
@@ -12,3 +13,6 @@ f = fontforge.open('ansi-blocks.sfd')
 # Generate the fonts
 f.generate('dist/ansi-blocks.ttf')
 f.generate('dist/ansi-blocks.woff2')
+
+# Copy the CSS target
+shutil.copyfile('ansi-blocks.css', 'dist/ansi-blocks.css')
